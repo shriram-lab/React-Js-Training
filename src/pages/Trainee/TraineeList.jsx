f3a1144349c92b934688f8704636326695c4c6b4
shriram <shriram.k@gmail.com>
2019-10-04 17:52:50 +0530

    Edit and delete and pagination some part

--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -21,6 +21,31 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { getDateFormatted, ascSort, descSort } from "../../configs/constants";
 import TableSortLabel from "@material-ui/core/TableSortLabel";
+import TablePagination from "@material-ui/core/TablePagination";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import EmailIcon from "@material-ui/icons/Email";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import { TextField } from "formik-material-ui";
+import { Formik, Field, Form } from "formik";
+import * as yup from "yup";
+
+const TranieeSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(3)
+    .required("Name is required field."),
+  email: yup
+    .string()
+    .email("Email should be valid.")
+    .required("Email is required field")
+});
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -43,7 +68,10 @@ class TranieeList extends Component {
       open: false,
       order: "asc",
       orderBy: "",
-      trainees: trainees
+      trainees: trainees,
+      editOpen: false,
+      deleteOpen: false,
+      id: ""
     };
   }
   handleClickOpen = event => {
@@ -54,7 +82,10 @@ class TranieeList extends Component {
 
   handleClose = event => {
     this.setState({
-      open: false
+      open: false,
+      deleteOpen: false,
+      editOpen: false,
+      id: ""
     });
   };
 
@@ -83,6 +114,44 @@ class TranieeList extends Component {
   handleSelect = event => {
     this.props.history.push(`/Trainee/${event}`);
   };
+
+  handleEdit = event => {
+    const editData = trainees.filter(person => person.id == event);
+    this.setState({
+      editOpen: true,
+      id: editData
+    });
+  };
+  handleDelete = event => {
+    this.setState({
+      deleteOpen: true,
+      id: event
+    });
+  };
+
+  deleteRecord = event => {
+    const delData = trainees.filter(person => person.id == event);
+    console.log("Deleted item", delData[0]);
+    this.setState({
+      deleteOpen: false,
+      id: ""
+    });
+  };
+  onSubmit = event => {
+    const editdata = trainees.filter(person => {
+      if (person.id == event.id) {
+        person.name = event.name;
+        person.email = event.email;
+        return person;
+      }
+    });
+    console.log("Trainee edited succesfully.", editdata[0]);
+    this.setState({
+      editOpen: false,
+      id: ""
+    });
+  };
+
   render() {
     const listtrainee = trainees.map(items => (
       <li>
@@ -106,6 +175,19 @@ class TranieeList extends Component {
             onClose={this.handleClose}
             onSubmit={this.handleSubmit}
           />
+          <DeleteDialog
+            open={this.state.deleteOpen}
+            handleClose={this.handleClose}
+            id={this.state.id}
+            deleteRecord={this.deleteRecord}
+          />
+          <EditDialog
+            open={this.state.editOpen}
+            handleClose={this.handleClose}
+            id={this.state.id}
+            deleteRecord={this.deleteRecord}
+            onSubmit={this.onSubmit}
+          />
           <br />
           <TableData
             id="name"
@@ -126,6 +208,16 @@ class TranieeList extends Component {
                 label: "Date",
                 align: "right",
                 format: getDateFormatted
+              }
+            ]}
+            actions={[
+              {
+                icon: <EditIcon />,
+                handler: this.handleEdit
+              },
+              {
+                icon: <DeleteIcon />,
+                handler: this.handleDelete
               }
             ]}
           />
@@ -162,21 +254,191 @@ function TableData(props) {
               key={row.name}
               hover
               style={{ cursor: "pointer" }}
-              onClick={() => onSelect(row.id)}
               className={index % 2 === 0 ? classes.stripped : null}
             >
               {props.columns.map(column => (
-                <TableCell component="th" scope="row" align={column.align}>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  align={column.align}
+                  onClick={() => onSelect(row.id)}
+                >
                   {column.format
                     ? column.format(row[column.field])
                     : row[column.field]}
                 </TableCell>
               ))}
+              {props.actions.map(action => (
+                <>
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    onClick={() => action.handler(row.id)}
+                  >
+                    {action.icon}
+                  </TableCell>
+                </>
+              ))}
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <TablePagination
+        // rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={100}
+        rowsPerPage={5}
+        page={1}
+        backIconButtonProps={{
+          "aria-label": "previous page"
+        }}
+        nextIconButtonProps={{
+          "aria-label": "next page"
+        }}
+        onChangePage={"handleChangePage"}
+        onChangeRowsPerPage={"handleChangeRowsPerPage"}
+      />
     </Paper>
+  );
+}
+
+function EditDialog(props) {
+  let name = "";
+  let email = "";
+  let id = "";
+  if (props.id) {
+    name = props.id[0].name;
+    email = props.id[0].email;
+    id = props.id[0].id;
+  }
+  return (
+    <>
+      <div>
+        <Dialog
+          open={props.open}
+          onClose={props.handleClose}
+          aria-labelledby="form-dialog-title"
+          fullWidth
+        >
+          <DialogTitle id="form-dialog-title">Edit Trainee</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Enter Trainee Details</DialogContentText>
+            <Formik
+              initialValues={{
+                name: name,
+                email: email,
+                id: id
+              }}
+              validationSchema={TranieeSchema}
+              onSubmit={values => props.onSubmit(values)}
+            >
+              {({ errors, touched, isValid, values, handleChange }) => (
+                <Form>
+                  <Field
+                    name="name"
+                    label="Name"
+                    type="text"
+                    fullWidth
+                    component={TextField}
+                    variant="outlined"
+                    required
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    margin="normal"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <AccountCircle />
+                        </InputAdornment>
+                      )
+                    }}
+                  ></Field>
+                  <Field
+                    name="email"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                    component={TextField}
+                    variant="outlined"
+                    required
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    margin="normal"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <EmailIcon />
+                        </InputAdornment>
+                      )
+                    }}
+                  ></Field>
+                  <DialogActions>
+                    <Button
+                      onClick={props.handleClose}
+                      variant="contained"
+                      color="primary"
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      onClick={props.handleClose}
+                      variant="contained"
+                      color="secondary"
+                      disabled={!isValid}
+                      type="submit"
+                    >
+                      Submit
+                    </Button>
+                  </DialogActions>
+                </Form>
+              )}
+            </Formik>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </>
+  );
+}
+
+function DeleteDialog(props) {
+  return (
+    <>
+      <div>
+        <Dialog
+          open={props.open}
+          onClose={props.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          fullWidth
+        >
+          <DialogTitle id="alert-dialog-title">{"Remove Trainee"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Are you really want to delete this traniee ?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={props.handleClose}
+              variant="contained"
+              color="primary"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={() => props.deleteRecord(props.id)}
+              variant="contained"
+              color="secondary"
+              autoFocus
+            >
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    </>
   );
 }
 
